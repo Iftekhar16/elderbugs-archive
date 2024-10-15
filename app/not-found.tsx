452a36b6d94f662129eed760520e6f85233ec372
Iftekhar16 <iftekharhossain16@gmail.com>
2024-10-15 23:25:19 +0600

    not found page update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const notFound = () => {
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center'>
-      <div className="title text-7xl font-semibold mb-3">404</div>
-      <div className="subtitle text-xl">Page not found</div>
+      <div className="title text-7xl font-semibold mb-3 text-accent2">404</div>
+      <div className="subtitle text-xl text-dark3">Page not found</div>
     </div>
   );
 };
